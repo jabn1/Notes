64832c406288cc880316d3096ff35789b33463ee
Jose Berges <jose.berges23@gmail.com>
2024-08-07 18:36:21 -0400

    Add OCR page

---
+++ Notes/Pages/OCR.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Tesseract;
+
+namespace Notes.Pages
+{
+    public class OCRModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        [Required]
+        [BindProperty]
+        public IFormFile? File { get; set; }
+
+        public async Task<IActionResult> OnPostUpload() 
+        {
+            if (File != null)
+            {
+                using var engine = new TesseractEngine(@"./tessdata", "spa", EngineMode.Default);
+
+                using var memoryStream = new MemoryStream();
+                await File.CopyToAsync(memoryStream);
+                memoryStream.Position = 0;
+
+                // have to load Pix via a bitmap since Pix doesn't support loading a stream.
+                using var image = new System.Drawing.Bitmap(memoryStream);
+
+                using var pix = PixConverter.ToPix(image);
+                using var resized = pix.Scale(3, 3);
+                using var gray = resized.ConvertRGBToGray();
+
+                using var inverted = gray.Invert();
+
+                using var page = engine.Process(inverted);
+
+                var meanConfidence = page.GetMeanConfidence();
+                var text = page.GetText();
+
+                if (!String.IsNullOrWhiteSpace(text))
+                {
+                    TempData["fileText"] = text.Trim();
+                    TempData["meanConfidence"] = meanConfidence.ToString();
+                }
+            }
+
+            return Redirect("/ocr");
+        }
+
+        
+    }
+}
--- Notes/wwwroot/js/site.js
+++ Notes/wwwroot/js/site.js
@@ -6,3 +6,16 @@
 function SelectNote(id) {
     document.getElementById('NewNoteParentId').value = id;
 }
+
+
+function CopyTextFromElement(id) {
+    var copyText = document.getElementById(id);
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+}
